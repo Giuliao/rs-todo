5491db352703bcff1d0c8ae287f250aa88f94b0f
Giuliao <wangguang16@outlook.com>
2024-05-11 11:30:50 +0800

    feat(json): 使用factroy统一生成任务&使用serde_json实现todo状态到文件的读写

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,18 @@
-mod todo;
+mod state;
 
-use todo::enums::TaskStatus;
-use todo::todo_factory;
-use todo::ItemTypes;
-
-use crate::todo::traits::delete::Delete;
-use crate::todo::traits::edit::Edit;
-use crate::todo::traits::get::Get;
+use serde_json::value::Value;
+use serde_json::{json, Map};
+use state::{read_file, write_to_file};
+use std::env;
 
 fn main() {
-    let todo_item = todo_factory("washing", TaskStatus::DONE);
-    match todo_item {
-        ItemTypes::Done(item) => {
-            item.get(&item.super_struct.title);
-            item.delete(&item.super_struct.title);
-        }
-        ItemTypes::Pending(item) => {
-            item.get(&item.super_struct.title);
-            item.set_to_done(&item.super_struct.title);
-        }
-    }
+    let args: Vec<String> = env::args().collect();
+    let status: &String = &args[1];
+    let title: &String = &args[2];
+    let mut state: Map<String, Value> = read_file("state.json");
+    println!("Before operation: {:?}", state);
+    state.insert(title.to_string(), json!(status));
+
+    println!("After operation: {:?}", state);
+    write_to_file("./state.json", &mut state);
 }
--- src/todo/traits/get.rs
+++ src/todo/traits/get.rs
@@ -1,5 +1,12 @@
+use serde_json::value::Value;
+use serde_json::Map;
+
 pub trait Get {
-    fn get(&self, title: &str) {
-        println!("Getting the task: {}", title);
+    fn get(&self, title: &String, state: &Map<String, Value>) {
+        let item: Option<&Value> = state.get(title);
+        match item {
+            Some(value) => println!("\n\nItem: {}", title),
+            None => println!("item: {} was not found", title),
+        }
     }
 }
